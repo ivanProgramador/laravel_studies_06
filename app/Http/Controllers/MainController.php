95edd0f3026ef4347d19fcc922dd7508dc870540
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-08-02 11:44:48 -0300

    inserindo dados com o eloquent modo unico e modo bulk

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,26 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index(){
 
+
+      Product::insert([
+        [
+         'product_name' => 'Produto 3',
+         'price' => 20,
+         'created_at' => Carbon::now(),
+         'updated_at' => Carbon::now()
+       ],
+       [
+         'product_name' => 'Produto 4',
+         'price' => 30,
+         'created_at' => Carbon::now(),
+         'updated_at' => Carbon::now()
+       ],
+       [
+         'product_name' => 'Produto 5',
+         'price' => 25,
+         'created_at' => Carbon::now(),
+         'updated_at' => Carbon::now()
+       ]   
+      ]);
+      
+      
+      
+
+      
         
        
 
-
-
-
-
-
-
-    
-
-      
-      
-    }
+}
 
 
     private function showData($data){
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+    //fillable é um array que diz quais campos podem ser preenchidos em massa
+    //isso é uma medida de segurança para evitar que campos indesejados sejam preenchidos
+    //se eu não colocar o fillable, o laravel não vai permitir que eu preencha esses campos
+    //então é importante sempre definir quais campos podem ser preenchidos em massa
     
+      protected $fillable = [
+        'product_name',
+        'price',
+        'created_at',
+        'updated_at'
+    ];
 }
